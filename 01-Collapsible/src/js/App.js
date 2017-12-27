c32aaba88e327112f5489bd1b8208e3814a3a421
Dmitriy Beglov <ditransler@gmail.com>
2017-12-27 21:58:18 +0400

    Finished R101#2: Passing children to React component

--- 01-Collapsible/src/js/App.js
+++ 01-Collapsible/src/js/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import image from '../images/expand-vertical-4.svg';
+import Collapsible from './Collapsible';
 
 class App extends React.Component {
     render() {
@@ -11,40 +12,20 @@ class App extends React.Component {
                 </header>
                 <div className="content">
                     <div className="panel-group">
-                        <div className="panel">
-                            <div className="panel-heading">
-                                <h2>Overview</h2>
-                            </div>
-                            <div className="panel-collapse">
-                                <div className="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.
-                                </div>
-                            </div>
-                        </div>
-                        <div className="panel">
-                            <div className="panel-heading">
-                                <h2>Features</h2>
-                            </div>
-                            <div className="panel-collapse">
-                                <div className="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.
-                                </div>
-                            </div>
-                        </div>
-                        <div className="panel">
-                            <div className="panel-heading">
-                                <h2>Reviews</h2>
-                            </div>
-                            <div className="panel-collapse">
-                                <div className="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.
-                                </div>
-                            </div>
-                        </div>
+                        <Collapsible title="Overview">
+                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.</p>
+                        </Collapsible>
+                        <Collapsible title="Features">
+                            <p>Esse dolore eu magna ut voluptate velit enim nisi ipsum qui eiusmod aute. Deserunt incididunt occaecat consequat nisi aliqua laborum esse. Adipisicing officia fugiat labore incididunt elit deserunt laborum qui nulla. Reprehenderit consequat exercitation officia do labore occaecat id. Culpa nisi nostrud proident nisi eu. Tempor laborum non proident ad commodo fugiat occaecat sit sunt velit. Laborum amet adipisicing exercitation magna officia culpa exercitation consequat laboris ex in.</p>
+                        </Collapsible>
+                        <Collapsible title="Software">
+                            <p>Esse commodo Lorem eiusmod non ipsum elit minim consectetur voluptate. Et incididunt aliquip Lorem ex labore proident. Eu pariatur est proident do est nostrud. Elit ea non aliquip elit et proident excepteur amet.</p>
+                        </Collapsible>
                     </div>
                 </div>
             </div>
         );
     }
 }
+
 export default App;
---
+++ 01-Collapsible/src/js/Collapsible.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Collapsible extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+
+    render() {
+        const {title, children} = this.props;
+
+        return (
+            <div className="panel">
+                <div className="panel-heading">
+                    <h2>{title}</h2>
+                </div>
+                <div className="panel-collapse">
+                    <div className="panel-body">
+                        {children}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+Collapsible.propTypes = {
+    title: PropTypes.string
+}
+
+export default Collapsible;
+
