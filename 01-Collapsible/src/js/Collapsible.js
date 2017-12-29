3dece36deda5d0c3bc70bbbf6d48796b745d95e3
Dmitriy Beglov <ditransler@gmail.com>
2017-12-29 19:54:24 +0400

    Finished R101#4: Calculating height of an element

--- 01-Collapsible/src/js/Collapsible.js
+++ 01-Collapsible/src/js/Collapsible.js
@@ -15,21 +15,23 @@ class Collapsible extends React.Component {
     handleToggle(evt) {
         evt.preventDefault();
         this.setState({
-            isExpanded: !this.state.isExpanded
+            isExpanded: !this.state.isExpanded,
+            height: this.refs.inner.clientHeight
         });
     }
 
     render() {
         const {title, children} = this.props;
-        const {isExpanded} = this.state;
+        const {isExpanded, height} = this.state;
+        const currentHeight = isExpanded ? height : 0;
 
         return (
             <div className={`panel ${isExpanded ? 'is-expanded' : ''}`} onClick={this.handleToggle}>
                 <div className="panel-heading">
                     <h2>{title}</h2>
                 </div>
-                <div className="panel-collapse">
-                    <div className="panel-body">
+                <div className="panel-collapse" style={{height: `${currentHeight}px`}}>
+                    <div className="panel-body" ref="inner">
                         {children}
                     </div>
                 </div>
